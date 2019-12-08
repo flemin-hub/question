ed104b9cf79794da1eda4456249c80e09f23fecb
Yachi Koei <logarim4401@gmail.com>
2019-12-08 09:28:58 +0000

    user model admin implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_152138) do
+ActiveRecord::Schema.define(version: 2019_12_08_091857) do
 
   create_table "agrees", force: :cascade do |t|
     t.integer "post_id"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 2019_12_05_152138) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
